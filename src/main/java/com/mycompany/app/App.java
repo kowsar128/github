4086988042266778a6cd7230eb07afb4aec0d299
kowsar128 <mohammadtasneem962@gmail.com>
2023-08-30 09:43:09 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Wellcome To DIET College!" );
+        System.out.println( "Wellcome To CSM Class!" );
     }
 }
