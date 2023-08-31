95cd0e98a4f851834b8e8d715c59220f9cb932a3
kowsar128 <142873312+kowsar128@users.noreply.github.com>
2023-08-31 15:19:57 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Wellcome To CSM Class!" );
+        System.out.println( "Wellcome To Jenkins Folder!" );
     }
 }
